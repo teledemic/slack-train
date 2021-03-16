421929cb2f20ddb4a7a79229951935eff4944abe
Carl Irving <carl@teledemic.com>
2021-03-16 15:58:52 -0400

    Fix user replace

--- convert.js
+++ convert.js
@@ -4,7 +4,7 @@ import path from "path";
 const SOURCE_PATH = "C:\\Users\\carl\\Downloads\\Vermontopia Slack export Mar 17 2020 - Mar 15 2021";
 const OUTPUT_PATH = "C:\\Users\\carl\\Downloads\\vermontopia training.txt";
 
-// const users = JSON.parse(await fs.readFile(path.join(SOURCE_PATH, "user.json")));
+const users = JSON.parse(await fs.readFile(path.join(SOURCE_PATH, "users.json")));
 
 const dirs = await fs.readdir(SOURCE_PATH);
 const output = fs.createWriteStream(OUTPUT_PATH);
@@ -15,13 +15,14 @@ for (const dir of dirs) {
 		for (const file of files) {
 			const json = JSON.parse(await fs.readFile(path.join(SOURCE_PATH, dir, file), "utf-8"));
 			for (const message of json) {
-				if (message.subtype !== undefined) continue;
-				// if (message.includes("<@")) {
-				// 	for (const user of users) {
-				// 		message = message.replace(user.id, user.name);
-				// 	}
-				// }
-				output.write(message.text + "\n");
+				if (message.subtype !== undefined || message.text === "") continue;
+				if (message.text.includes("<@")) {
+					for (const user of users) {
+						message.text = message.text.split(user.id).join(user.real_name);
+					}
+				}
+				const sender = users.find(item => item.id === message.user);
+				output.write(sender.real_name + ": " + message.text + "\n<|endoftext|>\n");
 			}
 		}
 	}
